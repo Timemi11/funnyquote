720eb35172d78e9776bbc5afb9fe98f7fbf854cf
Timemi11 <momuu56@gmail.com>
2023-07-23 09:35:28 +0700

    fix colors

--- lib/main.dart
+++ lib/main.dart
@@ -34,26 +34,59 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   static const List<String> quotes = [
-    "Avenger",
-    "Commics",
-    "Cartoon",
-    "Wanganda Forever",
-    "I like mama",
-    "ง่วงนอนจังเลยXD6",
-
+    "แคปชั่นไม่รู้ แต่แคบหมูไม่แน่",
+    "อยากมีคนใส่ใจ ที่ไม่ใช่ป้าข้างบ้าน",
+    "ใจไม่ดำหรอกจ้ะ ขอบตาต่างหากที่ดำ",
+    "อย่าทำให้หลงได้ไหม น้ำมันแพง",
+    "ไม่ค่อยหลายใจ ส่วนมากจะหลายขวด",
+    "เหมาะกับการอยู่ในวงเหล้า มากกว่าการเข้าไปอยู่ในใจใคร",
+  ];
+  static const List<Color> colors = [
+    Colors.amberAccent,
+    Colors.blue,
+    Colors.lightGreenAccent,
+    Colors.orangeAccent,
+    Colors.purpleAccent,
   ];
 
-    var quote = quotes[0];
+  var colorrnd = colors[0];
+  var quote = quotes[0];
+  var precolor = 0;
+  var preindex = 0;
+  var rad = Random();
 
-  void handleclick(){
-    setState(() { //refesh when imprement success
-      var rad = Random();
-      var rndindex =  rad.nextInt(quotes.length);
+  void handleclick() {
+    setState(() {
+      //refesh when imprement success
+      var rndindex = rad.nextInt(quotes.length);
+      if (rndindex == preindex) {
+        // print("it's match");
+        // print("before : ${rndindex}");
+        rndindex = rad.nextInt(quotes.length);
+        // print("after : ${rndindex}");
+        quote = quotes[rndindex];
+        preindex = rndindex;
+        return;
+      }
       quote = quotes[rndindex];
+      preindex = rndindex;
     });
   }
 
-
+  void randomcolor() {
+    setState(() {
+      //refesh when imprement success
+      var rndcolor = rad.nextInt(colors.length);
+      if (rndcolor == precolor) {
+        rndcolor = rad.nextInt(colors.length);
+        colorrnd = colors[rndcolor];
+        precolor = rndcolor;
+        return;
+      }
+      colorrnd = colors[rndcolor];
+      precolor = rndcolor;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -61,22 +94,26 @@ class _HomePageState extends State<HomePage> {
 
     return Scaffold(
       // Scaffold is widget help for layout
-      appBar: AppBar(title: const Text("kumkhomguanguan")),
+      appBar: AppBar(title: const Text("คำคมกวนๆ")),
       floatingActionButton: FloatingActionButton(
-        onPressed:handleclick ,
-        child: const Text("Go"),
+        onPressed: () {
+          //arrow function of dart
+          handleclick();
+          randomcolor();
+          //
+        },
+        child: const Text("สุ่มคำคม"),
       ),
       body: Padding(
         padding: const EdgeInsets.all(10.0),
         child: Center(
             child: Text(
           quote,
-          style: const TextStyle(
-            color: Colors.green,
+          style: TextStyle(
+            color: colorrnd,
             fontSize: 50,
             fontStyle: FontStyle.italic,
             fontWeight: FontWeight.w900,
-            letterSpacing: 20,
           ),
           textAlign: TextAlign.center,
         )),
